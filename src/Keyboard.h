ec96ea31dd060a3767881d23faaf203409abcecd
Steve Anderson <steve@twindx.com>
2022-04-05 16:56:46 +0100

    Update Keyboard.h

--- src/Keyboard.h
+++ src/Keyboard.h
@@ -113,6 +113,7 @@ extern const uint8_t KeyboardLayout_fr_FR[];
 extern const uint8_t KeyboardLayout_it_IT[];
 extern const uint8_t KeyboardLayout_sv_SE[];
 extern const uint8_t KeyboardLayout_da_DK[];
+extern const uint8_t KeyboardLayout_en_GB[];
 
 // Low level key report: up to 6 keys and shift, ctrl etc at once
 typedef struct
